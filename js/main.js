0085dbefea1c171c57175765ff5481949232288b
Anna Kaludzinska <anna.kaludzinska@gmail.com>
2016-12-13 18:30:23 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function() {
 	
+    $("#przycisk1").click(function(){
+        
+        $("p:last").insertBefore("p:first");
+        
+    });
+    
+    $("#przycisk2").click(function(){
+        
+        $("p:first").insertAfter("p:last");
+        
+    });
 	
 });
